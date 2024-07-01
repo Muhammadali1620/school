bd7347bc962c03d950a6d44af257f8947079a99d
Muhammadali1620 <muhammadvalievmuhammadali@gmail.com>
2024-07-01 18:33:27 +0500

    Payment List

--- apps/general/context_processors.py
+++ apps/general/context_processors.py
@@ -4,5 +4,5 @@ from django.db.models import Q
 
 def general(request):
     user_messages = Message.objects.filter(Q(chat__sender_id=request.user.id) |
-                                           Q(chat__recipient_id=request.user.id)).select_related('chat').order_by('-created_at')[0:5]
+                                           Q(chat__recipient_id=request.user.id)).order_by('-created_at')[0:5].select_related('chat')
     return {'user_messages': user_messages}
--- apps/payments/views.py
+++ apps/payments/views.py
@@ -1,5 +1,8 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from .models import Payment
+from django.views.generic import TemplateView, ListView
 
 
 class FeesCollectionTemplateView(TemplateView):
@@ -14,5 +17,7 @@ class AddExpenseTemplateView(TemplateView):
     template_name = "add-expense.html"
 
 
-class TeacherAllPayments(TemplateView):
+class TeacherAllPayments(ListView):
+    queryset = Payment.objects.filter(teacher__isnull=False).select_related('teacher', 'teacher__subject')
     template_name = "teacher-payment.html"
+    context_object_name = 'payments'
